70531ac442e6ae8147b841d82d2ab0b8ab4ebaa1
Yaakov <git@yaakov.online>
2020-05-21 17:38:01 +1000

    Also search for Notepad windows with changes.
    Fixes #3.

--- Notepad.Extensions.Logging/NotepadLogger.cs
+++ Notepad.Extensions.Logging/NotepadLogger.cs
@@ -84,9 +84,28 @@ namespace Microsoft.Extensions.Logging
 
         static void WriteToNotepad(string message)
         {
-            IntPtr hwnd = NativeMethods.FindWindow(null, "Untitled - Notepad");
+            IntPtr hwnd = FindNotepadWindow();
             IntPtr edit = NativeMethods.FindWindowEx(hwnd, IntPtr.Zero, "EDIT", null);
             NativeMethods.SendMessage(edit, NativeMethods.EM_REPLACESEL, (IntPtr)1, message);
+        }
+
+        static IntPtr FindNotepadWindow()
+        {
+            IntPtr hwnd;
+            
+            hwnd = NativeMethods.FindWindow(null, "Untitled - Notepad");
+            if (hwnd != IntPtr.Zero)
+            {
+                return hwnd;
+            }
+
+            hwnd = NativeMethods.FindWindow(null, "*Untitled - Notepad");
+            if (hwnd != IntPtr.Zero)
+            {
+                return hwnd;
+            }
+
+            return IntPtr.Zero;
         }
     }
 
